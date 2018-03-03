43f2ba69c9c9ec1628e12817df25d92f5c774f6b
ivanc <ivancardillo1996@gmail.com>
2018-03-03 15:54:15 +0100

    Sistemazione Registrazione

---
+++ app/src/main/java/io/eklesia/eklesia/IncontriFragment.java
@@ -0,0 +1,29 @@
+package io.eklesia.eklesia;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IncontriFragment extends Fragment {
+
+
+    public IncontriFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_incontri, container, false);
+        return view;
+    }
+
+}
